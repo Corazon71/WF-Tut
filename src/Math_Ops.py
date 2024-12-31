428bbb321e29a8a736e8e8ec6a9c7efe9b9bf5c5
Gurunath Salve <gurunaths6102003@gmail.com>
2024-12-31 09:37:42 +0530

    Should Work Now

--- src/Math_Ops.py
+++ src/Math_Ops.py
@@ -8,7 +8,12 @@ def CmplxProd(a, b):
     return (a['real'] * b['real'] - a['imag'] * b['imag'], a['real'] * b['imag'] + a['imag'] * b['real'])
 
 def CmplxQuot(a, b):
+    def round_up(value, decimals):
+        factor = 10 ** decimals
+        return ((value * factor) + 0.5) // 1 / factor
+
     den = b['real']**2 + b['imag']**2
     real_part = (a['real'] * b['real'] + a['imag'] * b['imag']) / den
     imag_part = (a['imag'] * b['real'] - a['real'] * b['imag']) / den
-    return round(real_part, 2), round(imag_part, 2)
+
+    return round_up(real_part, 2), round_up(imag_part, 2)
