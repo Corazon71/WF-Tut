72ec242ea198fac2822322ab28e0723bd3166242
Gurunath Salve <gurunaths6102003@gmail.com>
2024-12-31 09:31:24 +0530

    Code Refactored

--- src/Math_Ops.py
+++ src/Math_Ops.py
@@ -1,12 +1,12 @@
 def CmplxSum(a, b):
-    return (a.real + b.real, a.imag + b.imag)
+    return (a['real'] + b['real'], a['imag'] + b['imag'])
 
 def CmplxDiff(a, b):
-    return (a.real - b.real, a.imag - b.imag)
+    return (a['real'] - b['real'], a['imag'] - b['imag'])
 
 def CmplxProd(a, b):
-    return (a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real)
+    return (a['real'] * b['real'] - a['imag'] * b['imag'], a['real'] * b['imag'] + a['imag'] * b['real'])
 
 def CmplxQuot(a, b):
-    den = b.real**2 + b.imag**2
-    return ((a.real * b.real + a.imag * b.imag) / den, (a.imag * b.real - a.real * b.imag) / den)
+    den = b['real']**2 + b['imag']**2
+    return ((a['real'] * b['real'] + a['imag'] * b['imag']) / den, (a['imag'] * b['real'] - a['real'] * b['imag']) / den)
